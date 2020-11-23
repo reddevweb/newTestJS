93c1a51d6a7e60844545e468d3366ba3c11ddd77
reddevweb <reddevweb@gmail.com>
2020-11-24 00:47:12 +0300

    add for iterating any objects

---
+++ red.js
@@ -0,0 +1,23 @@
+" use strict ";
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+console.log(options.name);
+
+for( let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    }else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
